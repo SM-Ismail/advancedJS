a5d3f37e3f67404fa32fb968add130f99a5dbec2
Sayem Mohammad Ismail <sayemmohammadismail@gmail.com>
2021-02-04 19:21:53 +0600

    commit

---
+++ undefined-null.js
@@ -0,0 +1,22 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1+num2);
+}
+const result = add(1,2);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(3);
+console.log(result);
+
+const premik = {name:'rafi', phone:'12341234'};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+// null is absence of thing. It is explicitly set for a variable that something was in it but is empty for now.
