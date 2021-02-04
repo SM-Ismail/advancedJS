7105e46110da0c81843221ca4a77da31beaf9878
Sayem Mohammad Ismail <sayemmohammadismail@gmail.com>
2021-02-04 15:45:40 +0600

    commit

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+// conditions false- true
+
+/////falsy
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+/////truthy
+// true
+// '0'
+// " "
+// []
+// {}
+
+let nname = 0;
+if(nname || nname == 0){
+    console.log('condition is true');
+}else{
+    console.log('condition is false');
+}
+
